a129df6c6f5d8d2722be4947058fe205674f45cb
Puneet_s <puneet.as@leucinerichbio.com>
2020-01-19 20:56:49 +0530

    Filter extended

--- Lecture12/beginning-of-lecture-code/app.js
+++ Lecture12/beginning-of-lecture-code/app.js
@@ -8,6 +8,7 @@ MsgController.$inject = ['$scope', '$filter'];
 function MsgController($scope, $filter) {
   $scope.name = "Yaakov";
   $scope.stateOfBeing = "hungry";
+  $scope.cookieCost = .45;
 
   $scope.sayMessage = function () { 
     var msg = "Yaakov likes to eat healthy snacks at night!";
